965cf7d6305cbecea8bba8c09cbf0a7fba8deb48
Jared Keh <keh19001@byui.edu>
2025-12-13 10:53:52 -0700

    react is back

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { User, Note, Message, TabType } from './types';
 import { LoginForm } from './components/Auth/LoginForm';
 import { Header } from './components/Layout/Header';
